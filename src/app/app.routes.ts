68818bcf1de039a53664d07136eb60e0f8663aa3
Igor Dias <igorverissimo02@hotmail.com>
2024-10-03 13:19:01 -0300

    feat: update default angular screen to custom screem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+import { TextFormatterComponent } from './text-formatter/text-formatter.component';
+export const routes: Routes = [
+    { path: "textFormatter", component: TextFormatterComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ src/app/text-formatter/text-formatter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-formatter',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-formatter.component.html',
+  styleUrl: './text-formatter.component.css'
+})
+export class TextFormatterComponent {
+
+}
